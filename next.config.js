117994798a4aa02780eb2bb784eaf5b8af170763
Maxime-Le-Goff <lgmaximemlg@gmail.com>
2024-06-10 12:14:20 +0200

    song display

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+   images: {
+    domains: [
+        "awptnepmaevxssfnobga.supabase.co"
+    ]
+   } 
+}
 
 module.exports = nextConfig
